b4e02fd0cc230d5a14c49b8770a9d3e368da065f
tldrd0117 <tldrd0117@gmail.com>
2020-08-24 22:04:08 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+### Parcel-React-Template
+
+- bundler => parcel
+- transpiler => babel, typescript
+- uilibray => tailwind
+- styleling => postcss
+- linting => prettier
+- framework => react
+- test framework => jest
+- git hook => husky
---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { hot } from 'react-hot-loader';
+
+interface Props {
+    name: string;
+}
+
+class App extends React.Component<Props> {
+    render() {
+        const { name } = this.props;
+        return (
+            <>
+                <h1 className="text-4xl text-white bg-black">Hello {name}</h1>
+            </>
+        );
+    }
+}
+
+export default hot(module)(App);
